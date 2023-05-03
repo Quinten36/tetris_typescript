2037ab17a6bccf699dc58d4f22e0116df2eb0087
QuintenK <codestuff4all@gmail.com>
2023-05-03 15:57:09 +0200

    HTML/css start

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let message = '%cTypescript is running';
+console.log(message, 'color: green; font-size: 30px');
+// let heading = document.createElement('h1');
+// heading.textContent = message;
+// document.body.appendChild(heading);
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+let message: string = '%cTypescript is running';
+
+console.log(message, 'color: green; font-size: 30px')
+
+// let heading = document.createElement('h1');
+// heading.textContent = message;
+
+// document.body.appendChild(heading);
